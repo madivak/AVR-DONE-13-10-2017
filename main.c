a68782217da69f3ac76d297e9d7a3cd670ee42bd
Kevin Amadiva <madivak@live.co.uk>
2017-11-15 15:44:12 +0300

    cleaned main
    cleaned main

--- main.c
+++ main.c
@@ -25,9 +25,9 @@ static FILE uart0_input = FDEV_SETUP_STREAM(NULL, USART0_Receive, _FDEV_SETUP_RE
 int CheckSMS();
 void checkOKstatus();
 char sample_GPS_data (void);
-int CarStatus(int p);
 int sender();
 int CompareNumber();
+int initialstatus();
 
 #define CAR_ON	PORTB |= (1<<PORTB1)
 #define CAR_OFF	PORTB &= ~(1<<PORTB1)
@@ -35,12 +35,14 @@ int CompareNumber();
 int a; int i; char w; int y;
 char input;
 char buff[20];
-char company[]	= "NUMBER";
-char owner[]	= "NUMBER";
+char company[]	= "+25472xxxxxxx"; //moha's No#
+char company2[]	= "+25473xxxxxxx"; //fatah's no#
+char owner[]	= "+25475xxxxxxx"; //kevin's no#
+
 
 int main( void )
 {
-	CAR_ON;
+	CAR_OFF;
 	
 	USART1_Init(MYUBRR);
 	USART0_Init(MYUBRR);
@@ -50,9 +52,9 @@ int main( void )
  	stdout = &uart0_output;
 //	sei();
 	
-	_delay_ms(1000);
-/*	printf("\r\n STARTING SYSTEM \r\n");*/
-
+	
+	_delay_ms(13000);
+	initialstatus();
 	while(1) 
 	{
 		//check availability of SMS
@@ -60,18 +62,8 @@ int main( void )
 		CheckSMS(); //check if available unread SMS and its content
 		int f = buff[13]; // get car status value from buff[13]
 
-		//check SMS source
-		if (f < 3) // if SMS content is "1" or "0"
-		{ CompareNumber(); }  //check whether text no# is authorized + insert values to buff[14] & buff[15]
-		else
-		{ buff[14] = buff[15] = 0; } // if SMS content is neither "1" or "2"
-
-		//Alter status of car
-		int z = buff[14] + buff[15]; //sum values of the 2 buffer values
-		if (z < 2) //A scenario of receiving text from an authorized no# with '1' or '0'
-		{ CarStatus(f); }
-		else //A scenario of receiving text from Unauthorized no#
-		{}		
+ 		int y = buff[14] + buff[15] + buff[16]; //sum values of the 3 buffer values
+		
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////		
 		fdev_close();
 		stdout = &uart0_output;
@@ -90,7 +82,8 @@ int main( void )
 		}
 		printf("\r\nbuff[14] = %d", buff[14]);
 		printf("\r\nbuff[15] = %d", buff[15]);
-		printf("\r\nbuff[14] + buff[15] = %d", z);
+		printf("\r\nbuff[16] = %d", buff[16]);
+		printf("\r\nbuff[14] + buff[15] + buff[16] = %d", y);
 		printf("\r\nbuffer end\r\n");
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////		
 		fdev_close();
@@ -103,7 +96,7 @@ int main( void )
 
 int CheckSMS()
 {
-	//char w;
+	int z = 0; //char w;
 	y=0;
 	a=0;
 	printf("AT\r\n");
@@ -126,33 +119,68 @@ int CheckSMS()
 	if (w==0x02B) // if w = +
 	{
 		sender();
-		
 		w = getchar();
 		while (w !=  0x0A) //w is not <LF>
 		{ w = getchar();}
 		
 		w = getchar();
 		if (w == 0x030)//w is '0'
-		{ buff[13] = 1;  }
+		{	
+			CompareNumber();
+			z = buff[14] + buff[15] + buff[16]; //sum values of the 3 buffer values
+			if (z < 3) //A scenario of receiving text from an authorized no# with '1' or '0'
+				{	buff[13] = 1;
+					CAR_OFF; 
+				}
+			else //A scenario of receiving text from Unauthorized no#
+			{buff[13] = 0; }
+			  
+		}
 		else if(w == 0x031)//w is '1'
-		{ buff[13] = 2;  }
-		else{buff[13] = 6;}
+		{ 
+			CompareNumber();
+			z = buff[14] + buff[15] + buff[16]; //sum values of the 3 buffer values
+			if (z < 3) //A scenario of receiving text from an authorized no# with '1' or '0'
+			{	buff[13] = 2;
+				CAR_ON;
+			}
+			else //A scenario of receiving text from Unauthorized no#
+			{buff[13] = 0; }  
+		}
+		else{buff[13] = 6; buff[14] = buff[15] = buff[16] = 0; }
 	}
 	else if(w==0x04F) // if w = 'O'
 	{
 		w = getchar();
 		if (w==0x04B) // if w = 'K'
-		{ buff[13] = 3; }
+		{	buff[13] = 3; buff[14] = buff[15] = buff[16] = 0; 
+			initialstatus();
+		}
 		else
-		{ buff[13] = 4; }
+		{ buff[13] = 4; buff[14] = buff[15] = buff[16] = 0; }
+		
 	}
 	else
-	{buff[13] = 5;}
-	
-		if (buff[0]<3) // clear sms storage area if 0/1 is received
+	{buff[13] = 5; buff[14] = buff[15] = buff[16] = 0; }
+		
+	int E = buff[13];
+		if (E==1) // clear sms storage area if 0/1 is received
 		{
-			printf("AT+CMGF=1\r\n");
 			printf("AT+CMGD=1,4\r\n"); //clearing all SMS in storage AREA
+			checkOKstatus();
+			printf("AT+CMGW=\"2547xxxxxxxx\",145,\"STO UNSENT\"\r\n");
+			_delay_ms(2000);
+			printf("0");
+			putchar(0x1A); //putting AT-MSG termination CTRL+Z in USART0
+		}
+		else if (E==2) // clear sms storage area if 0/1 is received
+		{
+			printf("AT+CMGD=1,4\r\n"); //clearing all SMS in storage AREA
+			checkOKstatus();
+			printf("AT+CMGW=\"2547xxxxxxx\",145,\"STO UNSENT\"\r\n");
+			_delay_ms(2000);
+			printf("1");
+			putchar(0x1A); //putting AT-MSG termination CTRL+Z in USART0
 		}
 		else
 		{}
@@ -168,19 +196,6 @@ void checkOKstatus()
 			
 		while (w!=0x04B) // if w = K
 		{	w = getchar();	}
-}
-
-int CarStatus(int p)
-{
-	int c;
-	c = p;
-	if (c == 1)
-	{ CAR_OFF; }
-	else if (c == 2)
-	{ CAR_ON; }
-	else
-	{}
-	return 0;
 }
 
 int sender()
@@ -200,14 +215,18 @@ int sender()
 int CompareNumber()
 {
 	int j;
-	buff[14]=buff[15]=0;
+	buff[14]=buff[15]=buff[16]=0;
 	
 	for (j=0; j<13; j++)
 	{
 		if (buff[j]!=company[j])
 		{ buff[14] = 1;}
-		else if (buff[j]!=owner[j])
+		else{}
+		if (buff[j]!=owner[j])
 		{ buff[15] = 1;}
+		else{}
+		if (buff[j]!=company2[j])
+		{ buff[16] = 1;}
 		else{}
 	}
 	return *buff;
@@ -228,11 +247,11 @@ char sample_GPS_data (void)
 	_delay_ms(3000);
 	printf("AT+CIFSR\r\n");
 	_delay_ms(2000);
-	printf("AT+CIPSTART=\"TCP\",\"IP\",\"PORT\"\r\n");
+	printf("AT+CIPSTART=\"TCP\",\"SERVER_IP\",\"PORT\"\r\n");
 	_delay_ms(1000);
 	printf("AT+CIPSEND\r\n");
 	_delay_ms(2000);
-	printf("\r\n$GPGG");
+	printf("\r\nCAR PLATE NO:[KBY-3214] \r\nGPGG");
 	while(i == 0)
 	{
 		input = getchar();
@@ -257,10 +276,11 @@ char sample_GPS_data (void)
 							input = getchar();
 							while (input != 0x024)  //if the character is not "$"
 							{
-								putchar(input);
+								putchar(input); //Get GPGGA data
 								input = getchar();
-								i=1;
-							}
+							}	
+							i=1;
+							
 						}
 					}
 				}
@@ -270,10 +290,94 @@ char sample_GPS_data (void)
 		}
 		else{}
 	}
-//	printf("\r\nsStatus = %d", x);
+	
+	printf("GPRM");
+	while(i == 1)
+		{
+			input = getchar();
+			if (input == 0x024) //if the character is "$"
+			{
+				input = getchar();
+				if (input == 0x047) //if the character is "G"
+				{
+					input = getchar();
+					if (input == 0x050) //if the character is "P"
+					{
+						input = getchar();
+						if (input != 0x052) //if the character is not "R"
+						{}
+						else
+						{
+							input = getchar();
+							if (input != 0x04D) //if the character is not "M"
+							{}
+							else
+							{
+								input = getchar();
+								while (input != 0x024)  //if the character is not "$"
+								{
+									putchar(input); //Get GPGGA data
+									input = getchar();
+								}
+								i=2;
+							}
+						}
+					}
+					else{}
+				}
+				else{}
+			}
+			else{}
+		}
+	
+	printf("BUFF[13] = %d\r\n", buff[13]);
 	putchar(0x1A); //putting AT-MSG termination CTRL+Z in USART0
 	_delay_ms(2000);
 	printf("AT+CIPCLOSE\r\n");
 	_delay_ms(2000);
 	return 0;
+}
+
+int initialstatus()
+{
+	//char w;
+	y=0;
+	a=0;
+	printf("AT\r\n");
+	_delay_ms(2000);
+	
+	printf("AT+CMGF=1\r\n");
+	_delay_ms(2000);
+	
+	printf("AT+CMGR=1\r\n");
+	while (a < 2) //skip the <LF>
+	{
+		w = getchar();
+		if (w==0x0A)
+		{ a++; }
+		else
+		{}
+	}
+	w = getchar();
+	
+	if (w==0x02B) // if w = +
+	{
+//		sender();
+		
+		w = getchar();
+		while (w !=  0x0A) //w is not <LF>
+		{ w = getchar();}
+		
+		w = getchar();
+		if (w == 0x030)//w is '0'
+		{ buff[13] = 1; _delay_ms(200); CAR_OFF; }
+		else if(w == 0x031)//w is '1'
+		{ buff[13] = 2; _delay_ms(200); CAR_ON;  }
+		else{buff[13] = 6;}
+	}
+	
+	else
+	{}
+
+	return *buff;
 }
